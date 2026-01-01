7c8b1ef9cc618c38346acd9aa6f93e0caf9db3de
Balaji01-4D <j.balaji2468@gmail.com>
2026-01-01 18:43:23 +0530

    feat: add connector util

---
+++ internals/database/connector.go
@@ -0,0 +1,76 @@
+package database
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type Connector interface {
+	Connect(ctx context.Context) (*pgx.Conn, error)
+}
+
+type ConnStringConnector struct {
+	ConnString string
+}
+
+func NewConnStringConnector(connString string) *ConnStringConnector {
+	return &ConnStringConnector{
+		ConnString: connString,
+	}
+}
+
+func (c *ConnStringConnector) Connect(ctx context.Context) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(ctx, c.ConnString)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+type ConfigConnector struct {
+	Host     string
+	Database string
+	User     string
+	Password string
+	Port     uint16
+}
+
+func NewConfigConnector(host, database, user, password string, port uint16) *ConfigConnector {
+	return &ConfigConnector{
+		Host:     host,
+		Database: database,
+		User:     user,
+		Password: password,
+		Port:     port,
+	}
+}	
+
+func (c *ConfigConnector) Connect(ctx context.Context) (*pgx.Conn, error) {
+	connConfig, err := pgx.ParseConfig("")
+	if err != nil {
+		return nil, err
+	}
+
+	if c.Host != "" {
+		connConfig.Host = c.Host
+	}
+	if c.Database != "" {
+		connConfig.Database = c.Database
+	}
+	if c.User != "" {
+		connConfig.User = c.User
+	}
+	if c.Password != "" {
+		connConfig.Password = c.Password
+	}
+	if c.Port != 0 {
+		connConfig.Port = uint16(c.Port)
+	}
+
+	conn, err := pgx.ConnectConfig(ctx, connConfig)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}

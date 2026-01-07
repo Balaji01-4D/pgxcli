21a04bfecae06368750236e15eaa7e66660ba9cc
Balaji01-4D <j.balaji2468@gmail.com>
2026-01-07 22:48:34 +0530

    fix: reduntant connector for same work

--- internal/database/connector.go
+++ internal/database/connector.go
@@ -11,78 +11,45 @@ type Connector interface {
 	UpdatePassword(password string)
 }
 
-type ConnStringConnector struct {
-	ConnString string
+type PGConnector struct {
+	cfg *pgx.ConnConfig
 }
 
-func NewConnStringConnector(connString string) *ConnStringConnector {
-	return &ConnStringConnector{
-		ConnString: connString,
-	}
-}
-
-func (c *ConnStringConnector) UpdatePassword(newPassword string) {
-	cfg, err := pgx.ParseConfig(c.ConnString)
-	if err != nil {
-		return
-	}
-	cfg.Password = newPassword
-	c.ConnString = cfg.ConnString()
-}
-
-func (c *ConnStringConnector) Connect(ctx context.Context) (*pgx.Conn, error) {
-	conn, err := pgx.Connect(ctx, c.ConnString)
+func NewPGConnectorFromConnString(connString string) (*PGConnector, error) {
+	cfg, err := pgx.ParseConfig(connString)
 	if err != nil {
 		return nil, err
 	}
-	return conn, nil
+	return &PGConnector{cfg: cfg}, nil
 }
 
-type ConfigConnector struct {
-	Host     string
-	Database string
-	User     string
-	Password string
-	Port     uint16
-}
-
-func NewConfigConnector(host, database, user, password string, port uint16) *ConfigConnector {
-	return &ConfigConnector{
-		Host:     host,
-		Database: database,
-		User:     user,
-		Password: password,
-		Port:     port,
-	}
-}
-
-func (c *ConfigConnector) UpdatePassword(newPassword string) {
-	c.Password = newPassword
-}
-
-func (c *ConfigConnector) Connect(ctx context.Context) (*pgx.Conn, error) {
-	connConfig, err := pgx.ParseConfig("")
+func NewPGConnectorFromFields(host, database, user, password string, port uint16) (*PGConnector, error) {
+	cfg, err := pgx.ParseConfig("")
 	if err != nil {
 		return nil, err
 	}
+	checkAndSet := func(field *string, value string) {
+		if value != "" {
+			*field = value
+		}
+	}
+	checkAndSet(&cfg.Host, host)
+	checkAndSet(&cfg.Database, database)
+	checkAndSet(&cfg.User, user)
+	checkAndSet(&cfg.Password, password)
 
-	if c.Host != "" {
-		connConfig.Host = c.Host
+	if port != 0 {
+		cfg.Port = port
 	}
-	if c.Database != "" {
-		connConfig.Database = c.Database
-	}
-	if c.User != "" {
-		connConfig.User = c.User
-	}
-	if c.Password != "" {
-		connConfig.Password = c.Password
-	}
-	if c.Port != 0 {
-		connConfig.Port = uint16(c.Port)
-	}
+	return &PGConnector{cfg: cfg}, nil
+}
 
-	conn, err := pgx.ConnectConfig(ctx, connConfig)
+func (c *PGConnector) UpdatePassword(newPassword string) {
+	c.cfg.Password = newPassword
+}
+
+func (c *PGConnector) Connect(ctx context.Context) (*pgx.Conn, error) {
+	conn, err := pgx.ConnectConfig(ctx, c.cfg)
 	if err != nil {
 		return nil, err
 	}

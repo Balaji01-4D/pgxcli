15766c650ef6db7c0cfde75157f481e12a39c4cb
Balaji01-4D <j.balaji2468@gmail.com>
2026-01-11 19:17:18 +0530

    refactor: make the centralized

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,6 +2,9 @@ package cli
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"strings"
 
 	"github.com/balaji01-4d/pgxcli/internal/config"
 	postgres "github.com/balaji01-4d/pgxcli/internal/database"
@@ -10,13 +13,74 @@ import (
 
 const version = "v0.1.0"
 
-type pgxcli struct {
+type pgxCLI struct {
 	config *config.Config
 	client *postgres.Client
 	repl   *repl.Repl
 }
 
-func (p pgxcli) close(ctx context.Context) {
+func (p *pgxCLI) start(ctx context.Context, db, user string) error {
+
+	var connector postgres.Connector
+	var err error
+
+	if strings.Contains(db, "://") || strings.Contains(db, "=") {
+		connector, err = postgres.NewPGConnectorFromConnString(db)
+		if err != nil {
+			return fmt.Errorf("Invalid connection string: %v\n", err)
+		}
+	} else {
+		var password string
+
+		if opts.NeverPrompt {
+			password = os.Getenv("PGPASSWORD")
+		}
+
+		if opts.ForcePrompt && password == "" {
+			pwd, err := p.repl.ReadPassword()
+			if err != nil {
+				return fmt.Errorf("Failed to read password: %v\n", err)
+			}
+			password = pwd
+		}
+
+		connector, err = postgres.NewPGConnectorFromFields(
+			opts.Host,
+			db,
+			user,
+			password,
+			opts.Port,
+		)
+		if err != nil {
+			return fmt.Errorf("Failed to create connector: %v\n", err)
+		}
+	}
+
+	ConnErr := p.client.Connect(ctx, connector)
+	if ConnErr != nil {
+		if shouldAskForPassword(ConnErr, opts.NeverPrompt) {
+			pwd, err := p.repl.ReadPassword()
+			if err != nil {
+				return fmt.Errorf("Failed to read password: %v\n", err)
+			}
+			connector.UpdatePassword(pwd)
+			ConnErr = p.client.Connect(ctx, connector)
+			if ConnErr != nil {
+				return err
+			}
+		} else {
+			return ConnErr
+		}
+	}
+
+	if !p.client.IsConnected() {
+		return fmt.Errorf("Not connected to any database\n")
+	}
+
+	return nil
+}
+
+func (p *pgxCLI) close(ctx context.Context) {
 	p.client.Close(ctx)
 	p.repl.Close()
 }
--- internal/cli/runner.go
+++ internal/cli/runner.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/signal"
 	osuser "os/user"
-	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -17,11 +16,9 @@ import (
 )
 
 func run(cmd *cobra.Command, args []string) {
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	
 	var argDB string
 	var argUser string
 	if len(args) > 0 {
@@ -30,120 +27,57 @@ func run(cmd *cobra.Command, args []string) {
 	if len(args) > 1 {
 		argUser = args[1]
 	}
-	
-	finalDB, finalUser := resolveDBAndUser(opts.DBNameOpt, opts.UsernameOpt, argDB, argUser)
+
+	dbName, user := resolveDBAndUser(opts.DBNameOpt, opts.UsernameOpt, argDB, argUser)
 
 	// Load config
 	cfg := getConfig()
 
 	logger.InitLogger(opts.Debug, "logs/pgxcli.log")
 
-	postgres := database.New(ctx, cfg)
-	
-	r := repl.New(postgres, cfg)
+	postgres := database.New()
 
-	if finalUser == "" {
-		finalUser = os.Getenv("PGUSER")
-		if finalUser == "" {
+	app := pgxCLI{
+		config: cfg,
+		client: postgres,
+		repl:   repl.New(postgres, cfg),
+	}
+
+	if user == "" {
+		user = os.Getenv("PGUSER")
+		if user == "" {
 			currentUser, err := osuser.Current()
 			if err != nil {
-				r.PrintError(err)
+				app.repl.PrintError(err)
 				os.Exit(1)
 			}
-			finalUser = currentUser.Username
+			user = currentUser.Username
 		}
 	}
-	if finalDB == "" {
-		finalDB = finalUser
+	if dbName == "" {
+		dbName = user
 	}
-	
+
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
-	
+
 	go func() {
 		for {
 			<-sigChan
 		}
 	}()
 
-	appErr := startApplication(ctx, cfg, finalDB, finalUser)
+	appErr := app.start(ctx, dbName, user)
 	if appErr != nil {
-		r.PrintError(appErr)
-	}
-}
-
-func startApplication(ctx context.Context, cfg config.Config, db, user string) error {
-	postgres := database.New(ctx, cfg)
-	defer postgres.Close(ctx)
-
-	replClient := repl.New(postgres, cfg)
-	defer replClient.Close()
-
-	var connector database.Connector
-	var err error
-
-	if strings.Contains(db, "://") || strings.Contains(db, "=") {
-		connector, err = database.NewPGConnectorFromConnString(db)
-		if err != nil {
-			return fmt.Errorf("Invalid connection string: %v\n", err)
-		}
-	} else {
-		var password string
-
-		if opts.NeverPrompt {
-			password = os.Getenv("PGPASSWORD")
-		}
-
-		if opts.ForcePrompt && password == "" {
-			pwd, err := replClient.ReadPassword()
-			if err != nil {
-				return fmt.Errorf("Failed to read password: %v\n", err)
-			}
-			password = pwd
-		}
-
-		logger.Log.Debug("Connecting to database", "host", opts.Host, "port", opts.Port, "database", db, "user", user)
-		connector, err = database.NewPGConnectorFromFields(
-			opts.Host,
-			db,
-			user,
-			password,
-			opts.Port,
-		)
-		if err != nil {
-			return fmt.Errorf("Failed to create connector: %v\n", err)
-		}
+		app.repl.PrintError(appErr)
 	}
 
-	ConnErr := postgres.Connect(ctx, connector)
-	if ConnErr != nil {
-		if shouldAskForPassword(ConnErr, opts.NeverPrompt) {
-			pwd, err := replClient.ReadPassword()
-			if err != nil {
-				return fmt.Errorf("Failed to read password: %v\n", err)
-			}
-			connector.UpdatePassword(pwd)
-			ConnErr = postgres.Connect(ctx, connector)
-			if ConnErr != nil {
-				return err
-			}
-		} else {
-			return ConnErr
-		}
-	}
-
-	if !postgres.IsConnected() {
-		return fmt.Errorf("Not connected to any database\n")
-	}
-
-	replClient.Run(ctx)
-	replClient.Close()
-
-	return nil
+	app.close(ctx)
 }
 
 
-func getConfig() config.Config {
+
+func getConfig() *config.Config {
 	cfg := config.DefaultConfig
 	configDir, err := config.GetConfigDir()
 	if err != nil {
@@ -163,5 +97,5 @@ func getConfig() config.Config {
 			fmt.Fprintf(os.Stderr, "failed to save config %v\n", err)
 		}
 	}
-	return cfg
+	return &cfg
 }

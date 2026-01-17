5aab1d15d71b43c145cee565ea22afa41d417df1
Balaji01-4D <j.balaji2468@gmail.com>
2026-01-17 17:40:07 +0530

    test: add test for shouldAskPassword

---
+++ internal/cli/password_test.go
@@ -0,0 +1,49 @@
+package cli
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_shouldAskForPassword(t *testing.T) {
+	type args struct {
+		err         error
+		neverPrompt bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "not pgx error and never prompt",
+			args: args{err: fmt.Errorf("someError"), neverPrompt: true},
+			want: false,
+		},
+		{
+			name: "not pgx error and prompt",
+			args: args{err: fmt.Errorf("someError"), neverPrompt: false},
+			want: false,
+		},
+		{
+			name: "pgx invalid password error and never prompt",
+			args: args{err: &pgconn.PgError{Code: "28P01"}, neverPrompt: true},
+			want: false,
+		},
+		{
+			name: "pgx invalid password error and prompt",
+			args: args{err: &pgconn.PgError{Code: "28P01"}, neverPrompt: false},
+			want: true,
+		},  
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := shouldAskForPassword(tt.args.err, tt.args.neverPrompt)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}

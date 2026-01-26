44ee9ddb8dbcc9f889213766d3f195b5ef6b92a4
Balaji J <j.balaji4200@gmail.com>
2026-01-26 22:50:00 +0530

    test: add test for pager waitIgnoreEINTR
    Signed-off-by: Balaji J <j.balaji4200@gmail.com>

---
+++ internal/repl/pager_test.go
@@ -0,0 +1,46 @@
+package repl
+
+import (
+	"errors"
+	"syscall"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type fakeWaiter struct {
+	errs []error
+	i    int
+}
+
+func (fw *fakeWaiter) Wait() error {
+	err := fw.errs[fw.i]
+	fw.i++
+	return err
+}
+
+func Test_waitIgnoringInterrupt_IgnoreENTR(t *testing.T) {
+	fw := &fakeWaiter{
+		errs: []error{
+			syscall.EINTR,
+			syscall.EINTR,
+			nil,
+		},
+	}
+
+	err := waitIgnoringInterrupt(fw)
+	assert.Nil(t, err)
+}
+
+func Test_waitIgnoringInterrupt_ReturnOtherError(t *testing.T) {
+	someErr := errors.New("some error")
+	fw := &fakeWaiter{
+		errs: []error{
+			syscall.EINTR,
+			someErr,
+		},
+	}
+
+	err := waitIgnoringInterrupt(fw)
+	assert.Equal(t, someErr, err)
+}
